278bb09966ba616e2162d6ef7c7f2c944d8e5f77
thomasverney <mono_tape@yahoo.fr>
2018-10-06 21:33:26 +0100

    o

---
+++ Udemy/js/js.js
@@ -0,0 +1,8 @@
+
+var userName = prompt("What's your name?");
+var userlastName = prompt("What's your last name?");
+var userage = prompt("How old are you?");
+alert("Nice to meet you, "+ userName + " " + userlastName);
+alert("How does it feel to be "+userage+" years old?")
+console.log("Nice to meet you, "+ userName + " " + userlastName);
+console.log("How does it feel to be "+userage+" years old?")
