83d41743c8c5302046ccd36d21bb29c3cb9d55c9
thomasverney <mono_tape@yahoo.fr>
2018-10-07 21:30:30 +0100

    o

--- notes.js
+++ notes.js
@@ -1,8 +1,5 @@
 // Multi-line comments start with /* and end with */.
 
-// Arrays syntax uses brackets []:
-var cars = ["Saab", "Volvo", "BMW"];
-
 // An Expression is a set of combined values, like  these:
 "John" + " " + "Doe"
 5 * 10
@@ -10,4 +7,30 @@ var cars = ["Saab", "Volvo", "BMW"];
 // You can declare many variables in one statement:
 var person = "John Doe", carName = "Volvo", price = 200;
 
-console.log(2+2);
+// Arrays syntax uses brackets []:
+var cars = ["Saab", "Volvo", "BMW"];
+
+// ========================================================================================================================
+
+// Objects are written within {}. They can contain pairs of names and values. In this example, the array person contains 4 pairs of names and values:
+var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+
+// You can access each element in an Array by using its number. It starts with 0:
+cars[0];
+
+// ========================================================================================================================
+
+// for loops - Syntax:
+for ([initialization]; [condition]; [final - expression])
+    statement
+
+// example:
+var str = "";
+
+for (var i = 0; i < 9; i++) {
+    str = str + i;
+}
+
+console.log(str);
+// expected output: "012345678"
+// What it does is: "var i = 0" is the initial statement. Then, we indicate the condition, which is "i < 9". So as long as i is inferior to 9, the loop will keep running. Then, the "final - expression" is i++. It's like the result.
