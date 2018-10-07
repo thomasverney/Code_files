029bfcc0f4cab185786515e3ed943f31c328f280
thomasverney <mono_tape@yahoo.fr>
2018-10-07 10:42:55 +0100

    o

--- Ibat/week2/week2/exo perso.js
+++
@@ -1,2 +0,0 @@
-var cars = ["Saab", "Peugeot", "Renault", "BMW"];
-console.log(cars);
---
+++ notes.js
@@ -0,0 +1,13 @@
+// Multi-line comments start with /* and end with */.
+
+// Arrays syntax uses brackets []:
+var cars = ["Saab", "Volvo", "BMW"];
+
+// An Expression is a set of combined values, like  these:
+"John" + " " + "Doe"
+5 * 10
+
+// You can declare many variables in one statement:
+var person = "John Doe", carName = "Volvo", price = 200;
+
+console.log(2+2);
